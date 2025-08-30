7e2557580b92b1ea73bf89d2e7cbbf3e14d63397
Himanshu Raj <himanshuraj6771@gmail.com>
2025-08-31 01:09:54 +0530

    rbac added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,23 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "../config/db";
+import { admin } from "better-auth/plugins"
+import { ac, APP_ROLES } from "./permission";
+
 
 export const auth = betterAuth({
   trustedOrigins: [process.env.FRONTEND_URL as string],
   database: prismaAdapter(prisma, {
-    provider: "sqlite", // or "mysql", "postgresql", ...etc
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
   emailAndPassword: {
     enabled: true,
     autoSignIn: true
   },
+  plugins: [
+        admin({
+          ac,
+          roles: APP_ROLES
+        }) 
+    ]
 });
